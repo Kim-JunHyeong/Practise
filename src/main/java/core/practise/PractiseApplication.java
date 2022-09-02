f5636b50e163978be3aee4a30937dcf272dabb1d
Crazy <jhkim4685@gmail.com>
2022-09-02 17:39:19 +0900

    init project

---
+++ src/main/java/core/practise/PractiseApplication.java
@@ -0,0 +1,13 @@
+package core.practise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PractiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/core/practise/PractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package core.practise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
