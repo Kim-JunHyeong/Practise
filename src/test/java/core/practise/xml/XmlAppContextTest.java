825ad6a9bcd49fb5ecb7de395de9056a7da4d791
Crazy <jhkim4685@gmail.com>
2022-09-12 19:12:18 +0900

    test : 다양한 설정 형식, XML 형식 테스트

---
+++ src/test/java/core/practise/xml/XmlAppContextTest.java
@@ -0,0 +1,19 @@
+package core.practise.xml;
+
+import core.practise.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XmlAppContextTest {
+
+    @Test
+    void xmlAppContextTest() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
